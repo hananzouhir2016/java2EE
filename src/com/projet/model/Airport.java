68a9bc6f44cbfbf59d37e9297acfa51e6fd2dd45
lamiaa <misslamiaa19@gmail.com>
2021-01-28 17:44:24 +0100

    Aerop DAO, implement et bean

--- src/com/projet/model/Airport.java
+++ src/com/projet/model/Airport.java
@@ -1,5 +1,23 @@
 package com.projet.model;
 
 public class Airport {
+	
+	 private String nom;
+	 private Ville ville;
+	 
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Ville getVille() {
+		return ville;
+	}
+	public void setVille(Ville ville) {
+		this.ville = ville;
+	}
+	 
+	 
 
 }
